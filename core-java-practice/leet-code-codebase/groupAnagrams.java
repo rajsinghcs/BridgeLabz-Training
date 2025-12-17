36b8241e95d600d8885b148de158ba297b143756
rajsingcs <singhrajbahadur888@gmail.com>
2025-12-17 14:52:03 +0530

    Add new LeetCode solutions in leet-code-codebase

---
+++ core-java-practice/leet-code-codebase/groupAnagrams.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class groupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String s = new String(chars);
+
+            if (!map.containsKey(s)) {
+                map.put(s, new ArrayList<>());
+            }
+            map.get(s).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String args[]){
+        groupAnagrams sol = new groupAnagrams();
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> result = sol.groupAnagrams(strs);
+        System.out.println(result);
+    }
+}
---
+++ core-java-practice/leet-code-codebase/lengthOfLastWord.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class lengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        if(s.length() ==1){
+            return s.length();
+    }
+    int counter=0;
+    String t = s.trim();
+    for(int i=t.length()-1; i>=0; i--){
+        if(t.charAt(i) == ' '){
+           break;
+        }
+        else{
+            counter++;
+        }
+    }
+        return counter;
+    }
+    public static void main(String args[]){
+        lengthOfLastWord sol = new lengthOfLastWord();
+        String s = "Hello World";
+        System.out.println(sol.lengthOfLastWord(s)); 
+    }
+}
