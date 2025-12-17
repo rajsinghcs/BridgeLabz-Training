c99490da7c47117b37b65ea8a4acc3a56852f691
rajsinghcs <singhrajbahadur888@gmail.com>
2025-12-17 16:16:48 +0530

    [raj bahadur singh] added basic google classroom problems on day2

--- core-java-practice/gcr-codebase/programming-elements/addtwonumbers.java
+++ core-java-practice/gcr-codebase/programming-elements/addtwonumbers.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class addTwoNumbers {
+public class addtwonumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
