f0e9da42199596d1331cc35047ab4d54b621f79b
rajsingcs <singhrajbahadur888@gmail.com>
2025-12-17 09:11:21 +0530

    Add new LeetCode solutions in leet-code-codebase

---
+++ core-java-practice/leet-code-codebase/removeDuplicates.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        // Use a LinkedHashSet to store only unique values
+        Set<Integer> uniqueElements = new LinkedHashSet<>();
+        
+        // Traverse the array and add elements to the set
+        for (int num : arr) {
+            uniqueElements.add(num);
+        }
+
+        // Write the unique elements back into the original array
+        int index = 0;
+        for (int value : uniqueElements) {
+            arr[index++] = value;
+        }
+
+        // Return the number of unique elements
+        return uniqueElements.size();
+    }
+
+    // Main method to test the removeDuplicates function
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] testArray = {1, 1, 2, 3, 3, 4, 5, 5, 5};
+        
+        int newLength = solution.removeDuplicates(testArray);
+        
+        System.out.println("Number of unique elements: " + newLength);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(testArray[i] + " ");
+        }
+    }
+}
