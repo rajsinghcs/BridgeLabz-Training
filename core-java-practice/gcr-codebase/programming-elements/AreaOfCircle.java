61a584064f7d554d91d6525d64c54f2664d8c4c5
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-12-19 11:15:20 +0530

    Rename areaofcircle.java to AreaOfCircle.java

--- core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class areaofcircle {
+public class AreaOfCircle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the radius of the circle: ");
