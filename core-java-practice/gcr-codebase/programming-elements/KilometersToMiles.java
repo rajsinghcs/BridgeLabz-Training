1fd0d189c1b8deb4722606ae76bf470e9e7cab28
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-12-19 11:10:47 +0530

    Rename file to match Java naming conventions

--- core-java-practice/gcr-codebase/programming-elements/KilometersToMiles.java
+++ core-java-practice/gcr-codebase/programming-elements/KilometersToMiles.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class kilometersToMiles {
+public class KilometersToMiles {
     public static void main(String args[]){
         double kilometers = 10.8;
         double miles = kilometers * 0.621371;
