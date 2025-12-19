0834a62cdeb039b28071ea8a6356105638a2690e
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-12-19 11:13:16 +0530

    Rename file to ConvertKilometersToMiles.java

--- core-java-practice/gcr-codebase/programming-elements/ConvertKilometersToMiles.java
+++ core-java-practice/gcr-codebase/programming-elements/ConvertKilometersToMiles.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class convertKilometersTomiles {
+public class ConvertKilometersToMiles {
     public static void main(String[] args) {    
         Scanner scanner = new Scanner(System.in);
 
