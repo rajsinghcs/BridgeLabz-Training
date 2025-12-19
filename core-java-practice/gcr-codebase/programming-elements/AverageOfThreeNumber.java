88817c43594a8096bbfa801ed278251050d38856
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-12-19 11:14:41 +0530

    Rename file to AverageOfThreeNumber.java

--- core-java-practice/gcr-codebase/programming-elements/AverageOfThreeNumber.java
+++ core-java-practice/gcr-codebase/programming-elements/AverageOfThreeNumber.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class averageOfThreeNumber {
+public class AverageOfThreeNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
