11e1280fb49201d3a9cd3fae468a39ff17ad6383
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-12-19 12:03:46 +0530

    Rename longestCommonPrefix.java to LongestCommonPrefix.java

--- core-java-practice/leet-code-codebase/LongestCommonPrefix.java
+++ core-java-practice/leet-code-codebase/LongestCommonPrefix.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class longestCommonPrefix {
+public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         if(strs==null || strs.length == 0){
             return "";
