75d5b379bec766093f3fe1017aa7edd6ab5d7205
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-12-19 10:58:57 +0530

    Rename SimpleInterest.java file to match casing

--- core-java-practice/gcr-codebase/programming-elements/SimpleInterest.java
+++ core-java-practice/gcr-codebase/programming-elements/SimpleInterest.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class simpleInterest {
+public class SimpleInterest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
