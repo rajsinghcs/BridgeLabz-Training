cff450726e106b970b25ef1fdc44e23c16331783
Raj Bahadur Singh <122599400+rajsinghcs@users.noreply.github.com>
2025-12-19 11:07:02 +0530

    Rename welcomeToBridgeLabz.java to WelcomeToBridgeLabz.java

--- core-java-practice/gcr-codebase/programming-elements/WelcomeToBridgeLabz.java
+++ core-java-practice/gcr-codebase/programming-elements/WelcomeToBridgeLabz.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class welcomeToBridgeLabz {
+public class WelcomeToBridgeLabz {
     public static void main(String[] args) {
         System.out.println("Welcome to BridgeLabz");
     }
