367b8d5fe9a0fa9240703c70a8ea47b728a95f66
rajsinghcs <singhrajbahadur888@gmail.com>
2026-08-18 20:38:39 +0530

    [Raj Bahadur] Add.Added Day 11 payroll app

--- refresher-training/Day-8/contactApp/contactApp/src/main/java/com/contactApp/contactApp/controller/ContactAppController.java
+++ refresher-training/Day-8/contactApp/contactApp/src/main/java/com/contactApp/contactApp/controller/ContactAppController.java
@@ -1,15 +1,23 @@
 package com.contactApp.contactApp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.contactApp.contactApp.DTO.RequestDTO;
 import com.contactApp.contactApp.DTO.ResponseDTO;
 import com.contactApp.contactApp.entity.ContactApp;
 import com.contactApp.contactApp.service.ContactAppService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/contactApp")
@@ -20,6 +28,11 @@ public class ContactAppController {
 
     public ContactAppController(ContactAppService contactAppService) {
         this.contactAppService = contactAppService;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> welcomeMessage() {
+        return ResponseEntity.ok("Welcome to Contact App");
     }
 
 //    @PostMapping("/create")
